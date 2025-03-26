da5c87b9029814e6c01a21d9a5a34945837f3574
TaylorHyura <61947300+TaylorHyura@users.noreply.github.com>
2025-03-26 00:17:42 +0000

    Update mask handling in outfit screen

--- lua/mask_outfit_screen.lua
+++ lua/mask_outfit_screen.lua
@@ -1,5 +1,5 @@
-if not SYSHUD._settings.mask_outfit_screen then return end
-
 Hooks:PostHook(MenuSceneManager, "_set_up_templates", "unhide_mask_umu", function(self)
-	self._scene_templates.blackmarket_armor.hide_mask = false
+	if SYSHUD._settings.mask_outfit_screen then
+		self._scene_templates.blackmarket_armor.hide_mask = false
+	end
 end)
