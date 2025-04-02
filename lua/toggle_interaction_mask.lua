53cfae028ca1de9a35d2e369a367e7e774466500
TaylorHyura <61947300+TaylorHyura@users.noreply.github.com>
2025-04-02 03:21:52 +0100

    Fix toggleable settings naming in SYSTEM HUD

--- lua/toggle_interaction_mask.lua
+++ lua/toggle_interaction_mask.lua
@@ -1,7 +1,7 @@
 local PlayerMaskOff__check_use_item_original = PlayerMaskOff._check_use_item
 
 function PlayerMaskOff:_check_use_item( t, input )
-    if SYSHUD._settings.toggleble_mask then
+    if SYSHUD._settings.toggleable_mask then
         if input.btn_use_item_press and self._start_standard_expire_t then
             self:_interupt_action_start_standard()
             return false
